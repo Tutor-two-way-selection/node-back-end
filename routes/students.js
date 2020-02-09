e9d9203724efbff6448a6fef14c2120547a3b399
蔡鎔玖 <874088962@qq.com>
2020-02-09 21:54:15 +0800

    Update students.js

--- routes/students.js
+++ routes/students.js
@@ -1,15 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var studentservices = require("../api/studentApi");
-var fs=require("fs")
-var multer = require('multer');//引入multer
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/tmp/my-uploads')
-  }
-})
 
-var upload = multer({dest: 'uploads/'});//设置上传文件存储地址
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
@@ -22,14 +14,6 @@ router.get("/teacherbydepartment",studentservices.teacherByDepartment)
 router.get("/regular",studentservices.regularTeaSlected)
 router.get("/mytutor",studentservices.tutorResult)
 router.get("/selected",studentservices.selectedResult);
-router.post('/uploadFile', upload.single('file'), (req, res, next) => {
- 
-  
-  
-  let ret = {};
-  ret['code'] = 20000;
-  var file = req.file;
-  if (file) {
 
 
 
